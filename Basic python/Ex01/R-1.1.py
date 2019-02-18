a8c5522bbfe88109bc41b59ed8861828188dacad
Lawrence <ludingkun@outlook.com>
2019-02-18 17:18:03 +0800

    Create R-1.1.py

---
+++ Basic python/Ex01/R-1.1.py
@@ -0,0 +1,23 @@
+'''
+R-1.1 Write a short Python function, is multiple(n, m), that takes two integer
+values and returns True if n is a multiple of m, that is, n = mi for some
+integer i, and False otherwise.
+@author: lawrence  ludingkun@outlook.com
+@date: 2019-02-18
+'''
+
+def is_multiple():
+	n = input("Enter the first number N:")
+	m = input("Enter the first number M:")
+	n = int(n)
+	m = int(m)
+	if n%m == 0:
+		print("N is a multiple of M, and return: True")
+		return True
+	print("N is NOT a multiple of M, and return: False")
+	return False
+	
+
+if __name__ == "__main__":
+	is_multiple()
+
