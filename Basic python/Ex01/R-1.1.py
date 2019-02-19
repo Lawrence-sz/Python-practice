ef33fb4445c38a9f03bcc77cae7d7b334455198a
Lawrence <ludingkun@outlook.com>
2019-02-19 12:29:33 +0800

    Update R-1.1.py

--- Basic python/Ex01/R-1.1.py
+++ Basic python/Ex01/R-1.1.py
@@ -1,8 +1,8 @@
-'''
-R-1.1 Write a short Python function, is multiple(n, m), that takes two integer
+"""R-1.1 
+Write a short Python function, is multiple(n, m), that takes two integer
 values and returns True if n is a multiple of m, that is, n = mi for some
 integer i, and False otherwise.
-'''
+"""
 
 import string
 
@@ -15,7 +15,7 @@ def is_integer(var1):
     if tmp[0] == '-':
         tmp = tmp[1:]
 
-    # does it start with 0, or contain non-digits?
+    # does it start with 0, or contain non-digits?    
     if (tmp[0] == "0" and len(list(var1)) != 1) or not set(tmp) <= set(string.digits):
         print(f"--'{var1}'-- is not an integer number,")
         return False
@@ -23,7 +23,7 @@ def is_integer(var1):
     return True
 
 
-# define is_mutiple function
+# define is_mutiple function    
 def is_multiple():
     n = input("Enter the first integer number N:")
     # give the user another two tries
@@ -41,7 +41,9 @@ def is_multiple():
     for i in range(2):
         if not is_integer(m):
             m = input("Enter again! M:")
-            if i == 1 and (not is_integer(m) or list(m) == ['0']):
+			if list(m) == ['0']:
+				print("M should not be zero!")	
+            elif i == 1 and (not is_integer(m) or list(m) == ['0']):
                 print("You have tried for 3 times, we must quit this process!")
                 return False
         elif list(m) == ['0']:
