fb9082ba968b119e463f77750199c1c9700a167c
Lawrence <ludingkun@outlook.com>
2019-04-10 18:06:19 +0800

    Create phrase01-task01.py

---
+++ sit-team/phrase01-task01.py
@@ -0,0 +1,4 @@
+
+"""Define one string and one sub-string, and delete all matched sub-strings from the string in Python.（consider the recursive scenario and use function to do this job）
+e.g. string = ‘'abcababcbaaabbbb'’,  sub-string = ‘ab’.  Result = ‘ccbb’
+
